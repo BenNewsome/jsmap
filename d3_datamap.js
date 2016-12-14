827a7a34c04e9aad486db1d4ff22d7a71d48eac2
BEN5756 <bennewsome5756.bn@gmail.com>
2016-12-14 06:29:44 +0000

    Map not centered

--- d3_datamap.js
+++ d3_datamap.js
@@ -1,27 +1,62 @@
  <!-- map creation --> 
   // canvas resolution
-  var width = 1000,
-      height = 600;
+  var width = 800;
+  var height = 600;
+
+var width = document.body.clientWidth;
+var height = 0.5*width
+
+var scale = 100*(width/700)
  
   // projection-settings for mercator    
 //  var projection = d3.geo.mercator()
   var projection = d3.geo.equirectangular()
       // where to center the map in degrees
-      .center([0, 50 ])
+      .center([0, 0 ])
       // zoomlevel
-      .scale(100)
+      .scale(scale)
       // map-rotation
       .rotate([0,0]);
  
   // defines "svg" as data type and "make canvas" command
-  var svg = d3.select("body").append("svg")
+  
+  var title = d3.select("body")
+         .append("div")
+         .attr("id","title")
+         .attr("display", "block")
+         .attr("margin", "auto")
+         .append("p")
+         .attr("width",width)
+         .attr("height",height/5)
+         .style("text-align", "center")
+         .text("Title of Grapg")
+
+
+
+
+  var map = d3.select("body").append("div")
+         .attr("id", "mapArea")
+         .style("text-align","center")
+         .style("display","block")
+         .style("margin","auto")
+
+
+  var bar =  d3.select("body").append("div")
+//         .append("g")
+         .attr("id","colorbar")
+         .style("text-align","center")
+         ;
+
+
+  var svg = map.append("svg")
       .attr("width", width)
       .attr("height", height)
-      .attr("id", "map");
+      .attr("id", "map")
       ;
 
         var tooltip = d3.select('body').append('div')
             .attr('class', 'hidden tooltip');
+
  
   // defines "path" as return of geographic features
   var path = d3.geo.path()
@@ -39,6 +74,27 @@
             .domain([0,110])
 
    console.log( "color 25 = " + cbScale(25));
+
+
+    var colorbar = Colorbar()
+//            .origin([width/2,50])
+            .scale(cbScale)
+//            .orient("vertical")
+            .orient("horizontal")
+            .barlength(0.7*width)
+            .thickness(30)
+            ;
+
+    placeholder = "#colorbar-here";
+
+    colorbarObject = d3.select(placeholder)
+    .call(colorbar)
+    ;
+    
+
+  pointer = d3.selectAll("#colorbar").call(colorbar);
+
+
 
   var mySquare = {
       "type": "FeatureCollection",
@@ -84,15 +140,38 @@
     .style("fill", "blue")
 //    .attr("fill", function(d) {return cbScale(d.properties.conc)});
     ;
+
+
+function showLoader() {
+   document.getElementById('loader').style.visibility='visible';
+}
+
+function hideLoader() {
+   document.getElementById('loader').style.visibility='hidden';
+}
+
+
+function removeDataPoints() {
+   showLoader();
+   dataMap.selectAll("path").remove();
+   hideLoader();
+};
     
    function addDataPoints(jsonFileName) {
+   showLoader();
       d3.json(jsonFileName, function(error, jsonData) {
+           hideLoader();
          console.log("The json error is:");
          console.log(error);
          console.log("The json data is:");
          console.log(jsonData);
          console.log("The first point is: " + jsonData.features[0].geometry.coordinates)
          console.log("The first color is: " + jsonData.features[0].properties.conc)
+
+
+         // Set the title
+//         title.append("text").attr("x",width/2).attr("y", 0-(margin.top / 2)).text("My Ttitle")
+
 
 
          var dataPoints = dataMap.selectAll("path").data(jsonData.features)
@@ -106,10 +185,11 @@
                     var mouse = d3.mouse(svg.node()).map(function(d) {
                         return parseInt(d);
                     });
-                    tooltip.classed('hidden', false)
+  /*                  tooltip.classed('hidden', false)
                         .attr('style', 'left:' + (mouse[0] + 15) +
                                 'px; top:' + (mouse[1] - 35) + 'px')
                         .html(d.properties.name);
+ */                   pointer.pointTo(d.properties.conc);
                 })
                 .on('mouseout', function() {
                     tooltip.classed('hidden', true);
@@ -118,35 +198,16 @@
 });
 };
 
-var spinTarget = document.getElementById('map');
-var opts = {
-  lines: 9, // The number of lines to draw
-  length: 9, // The length of each line
-  width: 5, // The line thickness
-  radius: 14, // The radius of the inner circle
-  color: '#EE3124', // #rgb or #rrggbb or array of colors
-  speed: 1.9, // Rounds per second
-  trail: 40, // Afterglow percentage
-  className: 'spinner', // The CSS class to assign to the spinner
-};
-var spinner = new Spinner(opts).spin(spinTarget);
 
 
-function removeDataPoints() {
-   spinner.spin();
-   dataMap.selectAll("path").remove();
-   spinner.stop();
-};
 
   var addMap = d3.json("world-110m.json", function(error, topology) {
-      spinner.spin();
       worldMap.selectAll("path")
         .data(topojson.object(topology, topology.objects.countries)
             .geometries)
       .enter()
         .append("path")
         .attr("d", path);
-      spinner.stop();
   });
  
   // zoom and pan functionality
@@ -160,22 +221,23 @@ function removeDataPoints() {
  
   svg.call(zoom)*/
  
-    var colorbar = Colorbar()
-            .origin([25,50])
-            .scale(cbScale)
-            .orient("vertical")
-            .barlength(300)
-            .thickness(30)
 
-    placeholder = "#colorbar-here"
 
-    colorbarObject = d3.select(placeholder)
-    .call(colorbar)
 
-    bar =  d3.selectAll("svg").append("g").attr("id","colorbar")
+//    bar =  d3.selectAll("svg").append("g").attr("id","colorbar");
 
-    pointer = d3.selectAll("#colorbar").call(colorbar)
 
+/*
+   // Dropdown options
+   d3.select(".demoSelection")
+      .append("option")
+      .attr("value",1)
+      .text("Hello");
+   d3.select(".demoSelection")
+      .append("option")
+      .attr("value",2)
+      .text("Hello there");
+*/
 
 //    circles
 //        .on("mouseover",function(d) {pointer.pointTo(d[whichValue])})a
